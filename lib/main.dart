7b2955a748efa58340decfad5426efc4bed1d193
Leonardo Vinsen <leonardovinsen@gmail.com>
2022-06-30 13:45:56 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ void main() async {
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
 
+  debugPrint('abcdf');
+
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
